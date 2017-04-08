1f4b94816bf5562c0eec2d70287aafda384f005c
Kaven Peng <kavenp@student.unimelb.edu.au>
2017-04-08 23:24:08 +1000

    Fixed pprint issues, can print, but there are recursion bugs

--- snack/snack_pprint.ml
+++ snack/snack_pprint.ml
@@ -52,12 +52,6 @@ let op_prec expr =
   | _                       -> 8 
   (* Other exprs are all higher precedence *)
 
-let rec pr_comma_sep_list ppf pr_fun pr_list =
-  match pr_list with
-  | [] -> ()
-  | [l] -> pr_fun ppf l
-  | l :: ls -> pr_fun ppf l; fprintf ppf ", " ; pr_comma_sep_list ppf pr_fun ls
-
 (* Convert expression to string *)
 let rec pr_expr ppf expr =
   match expr with
@@ -74,7 +68,7 @@ pr_lval ppf lval =
   match lval with
   | LId ident -> fprintf ppf "%s" ident
   | LArray (ident, exprs) 
-    -> fprintf ppf "%s[%a]" (ident) (pr_comma_sep_list ppf pr_expr exprs)
+    -> fprintf ppf "%s[%a]" (ident) pr_comma_sep_exprs exprs
   (* Concat ident to front of a square bracketed list of comma separated expressions converted to string *)
 and
 (* Convert binary operations to string by
@@ -85,40 +79,54 @@ pr_binop_expr ppf lexpr binop rexpr =
   let mainExpr = Ebinop (lexpr, binop, rexpr)
   in
   fprintf ppf "%a %a %a" 
-  (bracket_binop ppf mainExpr lexpr)
-  (pr_binop ppf binop)
-  (bracket_binop ppf mainExpr rexpr ~isRight:true)
-  (*String.concat " " [bracket_binop pr mainExpr lexpr;
-                string_of_binop pr binop; 
-                bracket_binop pr mainExpr rexpr ~isRight:true]*)
+  bracket_binop_left mainExpr 
+  pr_binop binop 
+  bracket_binop_right mainExpr
 and
-(* Checks precendence in order to add brackets *)
-bracket_binop ppf expr ?isRight:(isRight=false) subexpr =
-  (* Add brackets if subexpression is lower precedence than main *)
-  if (op_prec subexpr) < (op_prec expr) then
-    fprintf ppf "(%a)" (pr_expr ppf subexpr)
+bracket_binop_left ppf (Ebinop (lexpr, binop, rexpr)) =
+  let expr = Ebinop (lexpr, binop, rexpr)
+  in
+  if (op_prec lexpr) < (op_prec expr) then
+    fprintf ppf "(%a)" pr_expr lexpr
   else
-  (* If subexpr is on right side and equal precedence to main expression
-   * we want to add brackets as well *)
-    if (op_prec expr = op_prec subexpr) && isRight then
-      fprintf ppf "(%a)" (pr_expr ppf subexpr)
-    else
-      pr_expr ppf subexpr
+    fprintf ppf "%a" pr_expr lexpr
+and
+bracket_binop_right ppf (Ebinop (lexpr, binop, rexpr)) = 
+  let expr = Ebinop (lexpr, binop, rexpr)
+  in
+  (* When right side is less than or equal in precedence to main expr
+   * we need to add brackets *)
+  if (op_prec rexpr) <= (op_prec expr) then
+    fprintf ppf "(%a)" pr_expr rexpr
+  else
+    fprintf ppf "%a" pr_expr rexpr
 and
 (* Convert unary operations to string
  * using bracket_unop to decide whether to add brackets or not 
  * depending on expression precedence *)
 pr_unop_expr ppf unop expr =
     match unop with
-    | Op_minus -> fprintf ppf "%a%a" (pr_unop unop) (bracket_unop ppf (Eunop (unop,expr)) expr)
-    | Op_not -> fprintf ppf "%a %a" (pr_unop unop) (bracket_unop ppf (Eunop (unop,expr)) expr)
+    | Op_minus -> fprintf ppf "%a%a" 
+                  pr_unop unop 
+                  bracket_unop (Eunop (unop,expr))
+    | Op_not   -> fprintf ppf "%a %a" 
+                  pr_unop unop 
+                  bracket_unop (Eunop (unop,expr))
 and
 (* Adds brackets to expression following unary op if its precedence is lower than main expression *)
-bracket_unop ppf expr subexpr =
-  if (op_prec subexpr) < (op_prec expr) then
-    fprintf ppf "(%a)" (pr_expr ppf subexpr)
+bracket_unop ppf (Eunop (unop, expr)) =
+  let mainExpr = Eunop (unop, expr)
+  in
+  if (op_prec expr) < (op_prec mainExpr) then
+    fprintf ppf "(%a)" pr_expr expr
   else 
-   fprintf ppf "%a" (pr_expr ppf subexpr)
+    fprintf ppf "%a" pr_expr expr
+and
+pr_comma_sep_exprs ppf exprs =
+  match exprs with
+  | [expr] -> fprintf ppf "%a" pr_expr expr
+  | expr :: es -> fprintf ppf ", %a" pr_expr expr ; pr_comma_sep_exprs ppf es
+  | [] -> ()
 
 let pr_rval ppf rval =
   match rval with
@@ -127,7 +135,13 @@ let pr_rval ppf rval =
 let pr_interval ppf inter =
   match inter with
   | Interval (low, high) ->
-    fprintf ppf "%d..%d" low high 
+    fprintf ppf "%d..%d" low high
+  
+let rec pr_comma_sep_inters ppf inters =
+  match inters with
+  | [inter] -> fprintf ppf "%a" pr_interval inter
+  | inter :: is -> fprintf ppf ", %a" pr_interval inter ; pr_comma_sep_inters ppf is
+  | [] -> ()
 
 let pr_snacktype ppf stype =
   match stype with
@@ -143,14 +157,14 @@ let pr_passtype ppf ptype =
 let pr_decl ppf decl =
   match decl with
   | RegDecl (id, stype) 
-    -> fprintf ppf "%a %s;" (pr_snacktype ppf stype) id
+    -> fprintf ppf "%a %s;" pr_snacktype stype id
   | ArrayDecl (id, stype, inters)
-    -> fprintf ppf "%a %s[%a];" (pr_snacktype ppf stype) id (pr_comma_sep_list ppf pr_interval inters)
+    -> fprintf ppf "%a %s[%a];" pr_snacktype stype id pr_comma_sep_inters inters
 
 let rec pr_decls ppf decls =
   match decls with
-  | [decl] -> fprintf ppf "@[<v>%a@;" (pr_decl ppf decl)
-  | decl :: ds -> fprintf ppf "%a@;" (pr_decl ppf decl) ; pr_decls ppf ds
+  | [decl] -> fprintf ppf "@[<v>%a@;" pr_decl decl
+  | decl :: ds -> fprintf ppf "%a" pr_decl decl ; pr_decls ppf ds
   | [] -> fprintf ppf "@]"
   (*(String.concat
   (String.concat "@;" (List.map (string_of_decl pr) decls))
@@ -160,26 +174,26 @@ let rec pr_stmts ppf stmts =
   let pr_stmt ppf stmt =
     match stmt with
     | Assign (lval, rval) 
-      -> fprintf ppf "%a := %a;" (pr_lval ppf lval) (pr_rval ppf rval)
+      -> fprintf ppf "%a := %a;" pr_lval lval pr_rval rval
       (*"(String.concat " " 
         [string_of_lval pr lval;":=";string_of_rval pr rval] ^
         ";")"*)
-    | Read lval -> fprintf ppf "read %a;" (pr_lval ppf lval)
+    | Read lval -> fprintf ppf "read %a;" pr_lval lval
     (*pr ("read " ^ string_of_lval pr lval ^ ";")*)
-    | Write expr -> fprintf ppf "write %a;" (pr_expr ppf expr)
+    | Write expr -> fprintf ppf "write %a;" pr_expr expr
     (*pr ("write " ^ string_of_expr pr expr ^ ";")*)
     | Ifthen (expr, stmts) -> pr_ifThen ppf expr stmts
     | IfthenElse (expr, thenStmts, elseStmts) 
       -> pr_ifThenElse ppf expr thenStmts elseStmts
     | WhileDo (expr, stmts) -> pr_whileDo ppf expr stmts
     | ProcCall (id, exprs) 
-      -> fprintf ppf "%s (%a)" id (pr_comma_sep_list ppf pr_expr exprs)
+      -> fprintf ppf "%s (%a)" id pr_comma_sep_exprs exprs
       (*(id ^ add_brackets (String.concat ", " 
         (List.map (string_of_expr pr) exprs)) ^ ";")*)
   in
   match stmts with
-  | [stmt] -> fprintf ppf "@[<v>%a@;" (pr_stmt ppf stmt)
-  | stmt :: ss -> fprintf ppf "%a@;" (pr_stmt ppf stmt)
+  | [stmt] -> fprintf ppf "@[<v>%a@;" pr_stmt stmt
+  | stmt :: ss -> fprintf ppf "%a@;" pr_stmt stmt
   | [] -> fprintf ppf "@]"
   (*(String.concat 
   (String.concat "@;" (List.map (string_of_stmt pr) stmts))
@@ -188,52 +202,56 @@ and
 pr_ifThen ppf expr stmts =
   fprintf ppf
   "@[<v>if %a then@;<0 4>%a@;fi@]" 
-  (pr_expr ppf expr) (pr_stmts ppf stmts)
+  pr_expr expr pr_stmts stmts
 and
 pr_ifThenElse ppf expr thenStmts elseStmts =
   fprintf ppf
   "@[<v>if %a then@;<0 4>%a@;else@;<0 4>%a@;fi@]"
-  (pr_expr ppf expr)
-  (pr_stmts ppf thenStmts) 
-  (pr_stmts ppf elseStmts)
+  pr_expr expr
+  pr_stmts thenStmts 
+  pr_stmts elseStmts
 and
 pr_whileDo ppf expr stmts =
   fprintf ppf
   "@[<v>while %a do@;<0 4>%a@;od@]"
-  (pr_expr ppf expr)
-  (pr_stmts ppf stmts)
+  pr_expr expr
+  pr_stmts stmts
 
 let pr_arg ppf (ptype, stype, id) =
-  fprintf ppf "%a %a %s" (pr_passtype ppf ptype) (pr_snacktype ppf stype) id  
+  fprintf ppf "%a %a %s" pr_passtype ptype pr_snacktype stype id  
   (*(String.concat " " 
   [string_of_passtype pr ptype;
   string_of_snacktype pr stype;
   id])*)
 
+let rec pr_comma_sep_args ppf args =
+  match args with
+  | arg :: ars -> fprintf ppf ", %a" pr_arg arg ; pr_comma_sep_args ppf ars
+  | [arg] -> fprintf ppf "%a" pr_arg arg
+  | [] -> fprintf ppf "()"
+
 let pr_args ppf args =
-  fprintf ppf "(%a)" (pr_comma_sep_list ppf pr_arg args)
+  fprintf ppf "(%a)" pr_comma_sep_args args
   (*pr (add_brackets (String.concat ", "
                (List.map (string_of_arg pr) args)))*)
 
 let pr_proc_body ppf (decls, stmts) =
-  pr
+  fprintf ppf
   ("@[<v>%a@;@;%a@]")
-  (pr_decls ppf decls)
-  (pr_stmts ppf stmts)
+  pr_decls decls
+  pr_stmts stmts
 
 let pr_proc ppf (id, args, proc_body) =
   fprintf ppf
-  "@[<v>proc %s %a@;<0 4>%a@;end"
-  id
-  (pr_args ppf args) 
-  (pr_proc_body ppf proc_body)
+  "@[<v>proc %s %a@;<0 4>%a@;end@]"
+  id pr_args args pr_proc_body proc_body
 
 let print_program ppf prog =
   let procs = prog.procs
   in
   match procs with
-  | [proc] -> fprintf ppf "@[<v>%a@;@;" (pr_proc ppf proc)
-  | proc :: ps -> fprintf ppf "%a@;@;" (pr_proc ppf proc)
+  | [proc] -> fprintf ppf "@[<v>%a@;@;" pr_proc proc
+  | proc :: ps -> fprintf ppf "%a@;@;" pr_proc proc
   | [] -> fprintf ppf "@]@."
   (*pr (String.concat 
   (String.concat "@;@;" (List.map (string_of_proc pr) procs))
